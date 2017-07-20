94bf4e36f558955a6eddb01aec34b2547e3d2b94
EamonBachari <eamonbachari@gmail.com>
2017-07-19 19:34:28 -0500

    Project complete

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import EarthquakeInfo from './EarthquakeInfo.js';
+import EarthquakeList from './EarthquakeList.js';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <div className="title">
+
+          <div className="my-header">
+            Earthquakes!
+          </div>
+          <EarthquakeInfo />
+          <EarthquakeList />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
